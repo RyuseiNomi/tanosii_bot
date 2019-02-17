c123118a1ed788f61aea9d8802eb50ca6efdfe0d
能美龍星 <nomiryuusei@nomiryuuseinoMacBook-puro.local>
2019-02-17 21:25:12 +0900

    switch文の修正

--- push_controller.php
+++ push_controller.php
@@ -3,7 +3,7 @@ require_once './push_model.php';
 
 class PushController
 {
-  
+
   public function fetchHttpHeader() {
       $fetched_http_header = PushModel::setHttpHeader();
 
@@ -11,9 +11,35 @@ class PushController
   }
 
   public function createPostData() {
-      $message = 'あたしンち';
+
+      date_default_timezone_set('Asia/Tokyo');
+      $current_date = date("d");
+
+      switch($current_date) {
+        case "17":
+            $message = "北海道旅行まであと2日です。";
+            break;
+        case "18":
+            $message = "北海道旅行まであと1日です。";
+            break;
+        case "19":
+            $message = "北海道旅行1日目から１年が経ちました。以下のドライブを参照して思い出を振り返りましょう。 https://www.amazon.co.jp/clouddrive/share/yWfXfqjY6X3mNGS3mD7QzFL78uuywDNI758ILYVvvpy/folder/sDFaXPM2Q6S7LBk7vp8c5Q?_encoding=UTF8&*Version*=1&*entries*=0&mgh=1";
+            break;
+        case "20":
+            $message = '北海道旅行2日目から１年が経ちました。以下のドライブを参照して思い出を振り返りましょう。 https://www.amazon.co.jp/clouddrive/share/yWfXfqjY6X3mNGS3mD7QzFL78uuywDNI758ILYVvvpy/folder/nWHMLSlqRHiZnFobj5YaiA?_encoding=UTF8&*Version*=1&*entries*=0&mgh=1';
+            break;
+        case "21": 
+            $message = '北海道旅行3日目から１年が経ちました。以下のドライブを参照して思い出を振り返りましょう。 https://www.amazon.co.jp/clouddrive/share/yWfXfqjY6X3mNGS3mD7QzFL78uuywDNI758ILYVvvpy/folder/kaOsCatOSamvHm8FBdSLdQ?_encoding=UTF8&*Version*=1&*entries*=0&mgh=1';
+            break;
+        case "22":
+            $message = '北海道旅行4日目から１年が経ちました。以下のドライブを参照して思い出を振り返りましょう。 https://www.amazon.co.jp/clouddrive/share/yWfXfqjY6X3mNGS3mD7QzFL78uuywDNI758ILYVvvpy/folder/gCiC4z_bQO6B8ocSvSJing?_encoding=UTF8&*Version*=1&*entries*=0&mgh=1';
+            break;
+        case "23":
+            $message = '北海道旅行5日目から１年が経ちました。以下のドライブを参照して思い出を振り返りましょう。 https://www.amazon.co.jp/clouddrive/share/yWfXfqjY6X3mNGS3mD7QzFL78uuywDNI758ILYVvvpy/folder/zwXUaRaXTnqNmQ3CWLS2xw?_encoding=UTF8&*Version*=1&*entries*=0&mgh=1';
+            break;
+      }
       $post = PushModel::setMessage($message);
-      
+
       return $post;
   }
 
@@ -31,10 +57,10 @@ class PushController
   }
 
   public function execCurl() {
-    
+
   }
 
   public function getHttpStatus() {
-    
+
   }
 }
