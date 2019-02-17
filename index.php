1d8fe7d5e07b809b869ebc538f586060003da355
能美龍星 <nomiryuusei@nomiryuuseinoMacBook-puro.local>
2019-02-17 21:35:42 +0900

    index.phpの修正

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-  file_get_contents('php://input'); 
+ob_start();
+$raw = file_get_contents('php://input');
+var_dump(json_decode($raw,1));
+//$raw = ob_get_clean();
+//file_put_contents('/tmp/dump.txt', $raw."\n=====================================\n", FILE_APPEND);
+
+echo "OK";
